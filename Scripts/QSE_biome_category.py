e2c1cbb5680e039b05854451cc7bda3dd9e3e8bf
priscillaahn <ahnpriscilla@gmail.com>
2020-12-10 01:39:30 -0500

    plot test

--- Scripts/QSE_biome_category.py
+++ Scripts/QSE_biome_category.py
@@ -2,6 +2,7 @@ import pandas as pd
 from pontiPy import *
 import os
 import glob
+import sys
 
 os.chdir("../DATA/TRANSITION")
 
@@ -12,6 +13,7 @@ def biome_contingency(time_interval = 1):
         # empty dataframe and lists
         _year, _category = [], []
         _quantity, _exchange, _shift  = [], [], []
+        _miss, _false_alarm = [], []
         df = pd.DataFrame(None)
         # get all csvs for biome
         d = dir + '/*.csv'
@@ -33,6 +35,16 @@ def biome_contingency(time_interval = 1):
                         _category.append(df.columns[col])
                         _year.append(fname_csv)
                         _quantity.append(round(obj.quantity(col)))
+                        _quan = obj.quantity(col, label = True)
+                        if "Miss" in _quan:
+                            _miss.append(round(_quan['Miss']))
+                            _false_alarm.append(0)
+                        elif "False Alarm" in _quan:
+                            _false_alarm.append(round(_quan['False Alarm']))
+                            _miss.append(0)
+                        else:
+                            _miss.append(0)
+                            _false_alarm.append(0)
                         _exchange.append(round(obj.exchange(col, Total = True)))
                         _shift.append(round(obj.shift(col)))
                 else:
@@ -48,13 +60,18 @@ def biome_contingency(time_interval = 1):
                 {'Year': _year,
                  'Category': _category,
                  'Quantity': _quantity,
+                 'Miss': _miss,
+                 'FalseAlarm': _false_alarm,
                  'Exchange': _exchange,
                  'Shift': _shift
                 })
+            # print(plot_df)
             # rearrange columns
-            plot_df = plot_df[['Year', 'Category', 'Quantity', 'Exchange', 'Shift']]
+            plot_df = plot_df[['Year', 'Category', 'Quantity', 'Miss', 'FalseAlarm','Exchange', 'Shift']]
             # save to file
             biome_name = '../BIOME_CONTINGENCY/CATEGORY/' + dir + '_' + str(time_interval) + 'year' + '.csv'
             plot_df.to_csv(biome_name)
+            # sys.exit()
+
 
 biome_contingency(1)
